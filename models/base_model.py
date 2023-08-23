af53e61e09c1464b9ed1cb713413a2c248eec10d
mitchellshera <mitchellwachera@gmail.com>
2023-08-23 19:08:40 +0300

    base

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
                     setattr(self, k, datetime.fromisoformat(kwargs[k]))
                 elif k != '__class__':
                     setattr(self, k, kwargs[k])
-            if storage_type ==  'db':
+            if storage_type == 'db':
                 if not hasattr(kwargs, 'id'):
                     setattr(self, 'id', str(uuid.uuid4()))
                 if not hasattr(kwargs, 'created_at'):
