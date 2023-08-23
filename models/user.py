6a5ccbac4cf328a21cc17ebcc4b8508f0cf74e1d
mitchellshera <mitchellwachera@gmail.com>
2023-08-23 23:18:23 +0300

    user

--- models/user.py
+++ models/user.py
@@ -8,16 +8,16 @@ from models import storage_type
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     if storage_type == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        # places = relationship('Place', backref='user',
-                              #cascade='all, delete, delete-orphan')
-        #reviews = relationship('Review', backref='user',
-                               #cascade='all, delete, delete-orphan')
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user",
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship("Review", backref="user",
+                               cascade='all, delete, delete-orphan')
     else:
         email = ""
         password = ""
