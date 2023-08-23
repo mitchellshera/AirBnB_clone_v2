38ff4f4942e375abc03d3762e2af281884fb5a02
mitchellshera <mitchellwachera@gmail.com>
2023-08-23 19:02:29 +0300

    user.py

--- models/user.py
+++ models/user.py
@@ -8,6 +8,7 @@ from models import storage_type
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     if storage_type == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
