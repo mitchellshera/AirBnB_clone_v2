ede50bf2fb8187105e7fe5f3b9de94ff36e748b2
mitchellshera <mitchellwachera@gmail.com>
2023-08-24 12:50:12 +0300

    g

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -26,7 +26,7 @@ class DBStorage:
         db = getenv('HBNB_MYSQL_DB')
         env = getenv('HBNB_ENV', 'development')
 
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format
+        self.__engine = create_engine('mysql+mysqldb://{user}:{pwd}@{host}/{db}'.format
                                       (user,pwd,host,db),
                                       pool_pre_ping=True)
         if env == 'test':
@@ -75,7 +75,7 @@ class DBStorage:
             )
         )
         self.__session = Session()
-    def close(self):
+def close(self):
         """ calls remove()
         """
         self.__session.close()
