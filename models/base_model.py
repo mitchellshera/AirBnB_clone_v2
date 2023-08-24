4eeffdf258ef770ae653d4b7f3d633f1485b97a0
mitchellshera <mitchellwachera@gmail.com>
2023-08-24 12:30:25 +0300

    state

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DATETIME
 import models
+
 Base = declarative_base()
 
 
--- models/state.py
+++ models/state.py
@@ -6,28 +6,24 @@ from models import storage_type
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import models
 
 class State(BaseModel, Base):
     """ State class / table model"""
     __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
-    else:
-        name = ''
         
-        @property
-        def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+    @property
+    def cities(self):
+        '''returns the list of City instances with state_id
+            equals the current State.id
+            FileStorage relationship between State and City
+        '''
+        related_cities = []
+        cities = models.storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                related_cities.append(city)
+        return related_cities
