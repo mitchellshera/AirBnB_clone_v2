ee6b7f79492122fd52ef51eac5dee938da146705
mitchellshera <mitchellwachera@gmail.com>
2023-08-24 09:57:13 +0300

    7

--- models/user.py
+++ models/user.py
@@ -23,3 +23,9 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email', '')
+        self.password = kwargs.get('password', '')
+        self.first_name = kwargs.get('first_name', '')
+        self.last_name = kwargs.get('last_name', '')
