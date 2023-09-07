10df37eb6911e091dc47844d1994f41f4bfd0298
mitchellshera <mitchellwachera@gmail.com>
2023-09-07 16:57:46 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,32 +8,7 @@ from datetime import datetime
 import os
 
 
-env.hosts = ['54.175.198.8', '52.86.56.129']
-
-def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    Returns: Archive path or None on failure.
-    """
-    try:
-        current_time = datetime.now()
-        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-            current_time.year,
-            str(current_time.month).zfill(2),
-            str(current_time.day).zfill(2),
-            str(current_time.hour).zfill(2),
-            str(current_time.minute).zfill(2),
-            str(current_time.second).zfill(2)
-        )
-        archive_path = "versions/{}".format(archive_name)
-
-        if not os.path.exists("versions"):
-            os.mkdir("versions")
-
-        local("tar -czvf {} web_static".format(archive_path))
-        return archive_path
-    except Exception:
-        return None
+env.hosts = ["54.175.198.8", "52.86.56.129"]
 
 def do_deploy(archive_path):
     """
