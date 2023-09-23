810762acfd5f1a13df2f30bd535b11c43fbaf05c
mitchellshera <mitchellwachera@gmail.com>
2023-09-23 23:10:38 +0300

    task 0 and 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-#shows hello hbnb!
+"""
+This script starts a Flask web application.
+
+The web application listens on 0.0.0.0 and port 5000.
+It defines a single route: '/' which displays "Hello HBNB!".
+The route definition uses the option strict_slashes=False.
+"""
 
 from flask import Flask
 
@@ -7,7 +13,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route that displays "Hello HBNB!" when accessed.
+    """
     return "Hello HBNB!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-# Start flask app
+"""
+This script starts a Flask web application.
+
+The web application listens on 0.0.0.0 and port 5000.
+It defines two routes:
+1. '/' which displays "Hello HBNB!".
+2. '/hbnb' which displays "HBNB".
+Both route definitions use the option strict_slashes=False.
+"""
 
 from flask import Flask
 
@@ -7,10 +15,16 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route that displays "Hello HBNB!" when accessed.
+    """
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Route that displays "HBNB" when accessed.
+    """
     return "HBNB"
 
 if __name__ == '__main__':
