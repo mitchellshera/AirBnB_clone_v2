8c61ebf610fdcbf66ec2bb9b261360ac444e085b
mitchellshera <mitchellwachera@gmail.com>
2023-09-23 23:33:13 +0300

    3 & 5

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -14,43 +14,39 @@ All route definitions use the option strict_slashes=False.
 """
 
 from flask import Flask
-from flask import escape
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """
-    Route that displays "Hello HBNB!" when accessed.
-    """
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """
-    Route that displays "HBNB" when accessed.
-    """
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
     """
-    Route that displays "C " followed by the value of the text variable
-    (replace underscore _ symbols with a space).
-    """
-    return "C {}".format(escape(text).replace('_', ' '))
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is_cool"):
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
     """
-    Route that displays "Python " followed by the value of the text variable
-    (replace underscore _ symbols with a space). The default value of text is "is cool".
-    """
-    return "Python {}".format(escape(text).replace('_', ' '))
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 if __name__ == '__main__':
